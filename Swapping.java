3b2c7a228ccad1a44538d6af7735f0bd8311ee0f
banaganiswapna666 <168511586+banaganiswapna666@users.noreply.github.com>
2025-06-17 09:57:04 +0530

    Add files via upload

---
+++ Swapping.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Swapping{
+	public static void main(String[] args){
+		Scanner sin=new Scanner(System.in);
+		System.out.println("enter a,b values:");
+		int a=sin.nextInt();
+		int b=sin.nextInt();
+		int c;
+		System.out.println("before swapping:");                                              );
+		a=b;
+		b=c;
+		c=a;
+		System.out.println("after swapping:");
+	}
+}
+		
